a215385447640414455a80fcb6ec30ebc2743bda
BigDaddyArrow <crewsquirrel@gmail.com>
2025-01-09 21:46:50 +0300

    initial commit + header in progress + prettier eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'portfolio-app';
+    title = 'portfolio-app';
 }
